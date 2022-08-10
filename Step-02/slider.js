7c963ea23c145942abd3f8e10371ac42c3b14ab4
Dima <pushokf@gmail.com>
2022-08-10 20:18:59 +0300

    added js to prev and next images

--- Step-02/slider.js
+++ Step-02/slider.js
@@ -1,1 +1,27 @@
-console.log('slider js');
+// function buttonChange(btnId) {
+//     const buttonElement = document.getElementById(btnId);
+
+//     buttonElement.addEventListener('click', function() {
+
+//         if(btnId == 'prev') {
+//             console.log('you clicked the previous button');
+//         } else if(btnId == 'next') {
+//             console.log('you clicked the next button');
+//         }
+//     })
+// };
+
+// buttonChange('prev');
+// buttonChange('next');
+
+
+function prevImg() {
+    console.log('You clicked on the previous image button');
+}
+
+function nextImg() {
+    console.log('You clicked on the next image button');
+}
+
+document.getElementById('prev').addEventListener('click', prevImg)
+document.getElementById('next').addEventListener('click', nextImg)
