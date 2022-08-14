78b35f50a4dbe7b7bacdf7bc90864367bcd9dcec
Dima <pushokf@gmail.com>
2022-08-14 21:17:59 +0300

    update

--- Step-02/slider.js
+++ Step-02/slider.js
@@ -1,22 +1,31 @@
 
-const imgArr = [
-    'OHR.HickmanBridge_ROW9566895207_1920x1080.jpg',
-    'OHR.LongsPeak_ROW9098186381_1920x1080.jpg',
-    'OHR.NoctilucentClouds_ROW9144099035_1920x1080.jpg',
-    'OHR.SpiralHill_ROW7328923046_1920x1080.jpg'
+const imagesArray = [
+    {
+        imageURL: 'OHR.HickmanBridge_ROW9566895207_1920x1080.jpg',
+    },
+    {
+        imageURL: 'OHR.LongsPeak_ROW9098186381_1920x1080.jpg',
+    },
+    {
+        imageURL: 'OHR.NoctilucentClouds_ROW9144099035_1920x1080.jpg',
+    },
+    {
+        imageURL: 'OHR.SpiralHill_ROW7328923046_1920x1080.jpg',
+    },
 ];
+
 
 let currentImage = 0;
 
 function renderImg() {
-    const imgURL = 'images/' + imgArr[currentImage];
+    const imgURL = 'images/' + imagesArray[currentImage].imageURL;
     document.getElementById('slider-image').src = imgURL;
 }
 
 function prevImg() {
     currentImage--;
-    if(currentImage < 0) {
-        currentImage = imgArr.length - 1;
+    if (currentImage < 0) {
+        currentImage = imagesArray.length - 1;
     }
     renderImg();
 
@@ -24,11 +33,36 @@ function prevImg() {
 
 function nextImg() {
     currentImage++;
-    if (currentImage > (imgArr.length - 1)) {
+    if (currentImage > (imagesArray.length - 1)) {
         currentImage = 0;
     }
     renderImg();
 }
 
-document.getElementById('prev').addEventListener('click', prevImg)
-document.getElementById('next').addEventListener('click', nextImg)
+let interval = null;
+
+function imageCycle() {
+    if (interval != null) {
+        return;
+    }
+
+    interval = setInterval(function () {
+        nextImg();
+    }, 1000);
+
+    console.log('interval', interval);
+};
+
+function stopImageCycle() {
+    clearInterval(interval);
+    interval = null;
+    console.log('stopImageCycle', interval);
+};
+
+document.getElementById('prev').addEventListener('click', prevImg);
+document.getElementById('next').addEventListener('click', nextImg);
+document.getElementById('cycle').addEventListener('click', imageCycle);
+document.getElementById('stop').addEventListener('click', stopImageCycle);
+
+
+
